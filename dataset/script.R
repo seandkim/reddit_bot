ee3264bac9cf5a0869d8ca6a3a0b712adccaacbb
Tiffany Lee <lyeukyu@gmail.com>
2017-09-09 13:20:37 -0400

    uploaded reddit datasets

---
+++ dataset/script.R
@@ -0,0 +1,30 @@
+
+# Define Path
+comments_dataset_path <- "Roastme-2017-03/comment.csv"
+post_dataset_path <- "Roastme-2017-03/post.csv"
+file_name <- "joint/Roastme-joint-2017-03.csv"
+
+# read library
+library(readr)
+library(dplyr)
+
+# Read data
+comment <- read_csv(comments_dataset_path)
+post <- read_csv(post_dataset_path)
+
+# Skim 
+comment_skimmed <- comment[, c("body", "link_id", "score")]
+post_skimmed <- post[, c("num_comments", "score", "title", "id")]
+
+link_id_truncated <- as.vector(sapply(comment_skimmed$link_id, substr, 4, 9))
+comment_skimmed$post_id <- link_id_truncated
+
+colnames(post_skimmed)[4] <- "post_id"
+
+# Join Table
+joint <- merge(post_skimmed, comment_skimmed, sort = FALSE)
+joint <- joint[, -6]
+
+# Write CSV
+write_csv(joint, file_name)
+

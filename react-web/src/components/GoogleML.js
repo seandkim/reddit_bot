ceb143f81c456f264d464c6049d092bf3d75148b
Sean Kim <seandkim14@gmail.com>
2017-09-09 09:23:27 -0400

    python gcloud api works

---
+++ react-web/src/components/GoogleML.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Language from '@google-cloud/language';
+
+export default class GoogleML extends Component {
+  constructor(props) {
+    super(props);
+
+
+    console.log("hi")
+  }
+
+  render() {
+    return (
+      <div>text</div>
+    );
+  }
+}

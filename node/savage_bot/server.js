e42000f56a1dc193e458520cfaa4f5077f0cbac1
Sean Kim <seandkim14@gmail.com>
2017-09-09 16:08:52 -0400

    gcloud + node communication works

--- node/savage_bot/server.js
+++ node/savage_bot/server.js
@@ -1,40 +1,52 @@
 var express = require('express')
 const Language = require('@google-cloud/language');
+var url = require('url');
 
 // start on port 3000
 var app = express()
 var http = require('http').Server(app)
+var io = require('socket.io')(http);
+const language = Language(); // Instantiates a gcloud client
 
 app.get('/', function(req, res) {
+  // console.log("query string = ", req.query) //respond to query
+
   res.sendFile(__dirname + '/index.html')
 })
 
-http.listen(3000, function() {
-  console.log("listening on port 3000")
-})
-
-
-// instantiate google cloud machine learning API
-
-
-
-// The text to analyze
-const text = 'Hello, world!';
-
-const document = {
-  'content': text,
-  type: 'PLAIN_TEXT'
-};
-
-// Detects the sentiment of the text
-language.analyzeSentiment({'document': document})
-  .then((results) => {
-    const sentiment = results[0].documentSentiment;
-
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+io.on('connection', function(socket) {
+  socket.on('post_analyze', function(post_text) {
+    console.log("post_analyze:", post_text);
+    gcloudAPI(post_text);
+    return false;
   })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
+
+  function gcloudAPI(text) {
+    // socket.emit('post_result', "hello");
+    console.log("gcloudAPI start");
+
+    const document = {
+      'content': text,
+      type: 'PLAIN_TEXT'
+    };
+
+    // Detects the sentiment of the text
+    language.analyzeEntities({'document': document})
+      .then((results) => {
+        const sentiment = results[0].documentSentiment;
+
+        console.log(`Text: ${text}`);
+        console.log(`Sentiment score: ${sentiment.score}`);
+        console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+
+        socket.emit('post_result', results);
+      })
+      .catch((err) => {
+        console.error('ERROR:', err);
+      });
+  }
+});
+
+http.listen(3000, function() {
+  console.log('listening on ' + 3000);
+});

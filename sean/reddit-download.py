48113d476288d2a14b4a6af232dd0d5cabb40205
Sean Kim <seandkim14@gmail.com>
2017-09-09 11:30:40 -0400

    added basic node

--- sean/reddit-download.py
+++ sean/reddit-download.py
@@ -1,15 +1,20 @@
 import urllib2 # make GET request
 from bs4 import BeautifulSoup
+import re # regex expression
 # soup = BeautifulSoup(html_doc, 'html.parser')
 
+
+
 def getRedditHome(subredit): 
+	# TODO retry after 429 error
 	html_doc = urllib2.urlopen("https://www.reddit.com/r/RoastMe/").read()
 	soup = BeautifulSoup(html_doc, 'html.parser')
-	res = soup.find_all('a', {})
-	print(res)
+	comment_links = soup.find_all('a', {'href': re.compile("^/r/RoastMe/comments/")})
+	
+	for link in comment_links:
+		html_doc = urllib2.urlopen(link).read()
+		print(html_doc.find('title'))
 
 	return 42
-
-
 
 getRedditHome("RoastMe")

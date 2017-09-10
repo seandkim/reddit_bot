e820d25915088da18bd605c12264673006c165ce
Sean Kim <seandkim14@gmail.com>
2017-09-10 02:25:56 -0400

    everything works on a dummy file

--- node/savage_bot/public/js/socket.js
+++ node/savage_bot/public/js/socket.js
@@ -1,18 +1,41 @@
 var socket = io();
-var DEFAULT_RESPONSE = 'As if I would care.'
+var DEFAULT_RESPONSES = [ "Okay. Good to know.",
+					"Sounds like something I would say if I didn't sleep for a week.",
+					"Staring at the fan spinning is probably more interesting than responding to you.",
+					"Hello darkness my old friend.",
+					"That's a nice move, but it's better if you didn't move.",
+					"See that blank wall? The decor matches your personality.",
+					"...You're probably an engineer.",
+					"I wanted to say something wholesome - but I have no desire now that when I see you.",
+					"What's your name again? Actually... nevermind, I don't want to know.",
+					"You look like you're always on your phone",
+					"Speak English, not code.",
+					"My proudest achievement is to talk to you without walking away.",
+					"You look like someone who would use a PC.",
+					"Adam and Eve would never eat the apple if they knew you would be their offspring.",
+					"Seems like you're lonely but you have no friends so you have to talk to me.",
+					"...You look like someone who would stil believe in Santa Claus when you're 20."]
 
 $(document).ready(function() {
+    function getRandomElem(L) {
+      //The maximum is exclusive and the minimum is inclusive
+      min = Math.ceil(0);
+      max = Math.floor(L.length);
+      idx = Math.floor(Math.random() * (max - min)) + min;
+      return L[idx]
+    }
+
     // listen for result
     socket.on('post_result', function(response) {
       if (response['success']) {
         const keywords = response['keywords']
         const comment = response['comment']
         console.log("result from server: ", keywords, comment);
-        $('#feedback').html(DEFAULT_RESPONSE)
+        $('#feedback').html(comment)
       } else {
         console.log("post_result socket call failed")
-        
-        $('#feedback').html(DEFAULT_RESPONSE)
+
+        $('#feedback').html(getRandomElem(DEFAULT_RESPONSES))
       }
     });
 });
--- node/savage_bot/server.js
+++ node/savage_bot/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 const Language = require('@google-cloud/language');
 var url = require('url');
+var fs = require('fs'); // for opening json file
 
 // start on port 3000
 var app = express()
@@ -11,10 +12,39 @@ const language = Language(); // Instantiates a gcloud client
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   // console.log("query string = ", req.query) //respond to query
-
   res.sendFile(__dirname + '/index.html')
 })
 
+// database handling
+// keyword => commentID
+var json = JSON.parse(fs.readFileSync('example.json', 'utf8'));
+// console.log("JSON is", json)
+var dict = new Map();
+for (var idx in json) {
+  var e = json[idx]
+  dict.set(e['keyword'], e['commentID'])
+}
+
+// commentID => comment
+var comments = fs.readFileSync('comments.csv', 'utf8').split('\n');
+
+// takes in a list of keywords
+// returns a best response from the database
+function searchDatabase(kws) {
+  function getRandomElem(L) {
+    //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(0);
+    max = Math.floor(L.length);
+    idx = Math.floor(Math.random() * (max - min)) + min;
+    return L[idx]
+  }
+
+  const kw = getRandomElem(kws)
+  const commentID = getRandomElem(dict.get(kw))
+  return comments[commentID]
+}
+
+// communication with html (client)
 io.on('connection', function(socket) {
   socket.on('post_analyze', function(post_text) {
     console.log("received post_analyze:", post_text);
@@ -31,7 +61,7 @@ io.on('connection', function(socket) {
     // Detects the sentiment of the text
     language.analyzeEntities({'document': document})
       .then((results) => {
-        console.log("analyzeEntities succeeded")
+        console.log("analyzeEntities succeeded", results)
         const kws = results[0]['entities'].map(function(e) {return e['name']})
         const comment = searchDatabase(kws);
 
@@ -44,7 +74,7 @@ io.on('connection', function(socket) {
         socket.emit('post_result', response);
       })
       .catch((err) => {
-        console.log("analyzeEntities failed")
+        console.log("analyzeEntities failed", err)
         const response = {
           success: false,
           keywords: null,
@@ -55,14 +85,6 @@ io.on('connection', function(socket) {
       });
   }
 });
-
-
-// takes in a list of keywords
-// returns a best response from the database
-function searchDatabase(kws) {
-  return "Your mom";
-}
-
 
 http.listen(3000, function() {
   console.log('listening on ' + 3000);
